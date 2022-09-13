c5e333aeef6f732a739787db3c90ba6a950fd90a
gullalc <gullalcheema@gmail.com>
2022-09-13 15:08:16 +0200

    add clip feature extraction

---
+++ extraction/feat_extract_clip.py
@@ -0,0 +1,71 @@
+import clip
+from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+
+import os, sys
+import numpy as np
+from PIL import Image
+import pandas as pd
+import json
+from tqdm import tqdm
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+sys.path.append('../MM_Claims/')
+from utils.preprocessing import *
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Extract CLIP features')
+parser.add_argument('-s', '--split', type=str, default='train',
+                    help='train | val | test')
+parser.add_argument('-c','--clip', type=str, default='rn50',
+                    help='rn50 | rn504 | vit16')
+
+args = parser.parse_args()
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+_tokenizer = _Tokenizer()
+
+model_nms = {'vit16':'ViT-B/16', 'rn50':'RN50', 'rn504':'RN50x4'}
+model_dim = {'vit16': 512, 'rn50': 1024, 'rn504': 640}
+
+## Arguments
+clip_nm = args.clip
+split = args.split
+
+print("Extracting features from:", model_nms[clip_nm], split,"-------------------------------")
+
+text_dict = json.load(open('data/%s_text.json'%(split), 'r', encoding='utf-8'))
+
+text_processor = get_text_processor(word_stats='twitter', keep_hashtags=True)
+
+im_names, img_feats, text_feats = [], [], []
+
+model, img_preprocess = clip.load(model_nms[clip_nm], device=device)
+
+for key in tqdm(text_dict.keys()):
+    text = text_dict[key]['tweet_text']
+    img = Image.open(os.path.join('data/images_labeled/', key+'.jpg'))  
+    
+    proc_text = process_tweet(text, text_processor)
+
+    image_tensor = img_preprocess(img).unsqueeze(0).to(device)
+    text_tokens = clip_tokenize(text, _tokenizer).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image_tensor)
+        text_features = model.encode_text(text_tokens)
+    
+        img_feats.append(image_features.cpu().numpy().flatten().tolist())
+        text_feats.append(text_features.cpu().numpy().flatten().tolist())
+        im_names.append(key)
+
+
+feat_dict = {}
+feat_dict['img_feats'] = {name: feat for name, feat in zip(im_names, img_feats)}
+feat_dict['text_feats'] = {name: feat for name, feat in zip(im_names, text_feats)}
+print(len(feat_dict['img_feats']), len(feat_dict['text_feats']))
+json.dump(feat_dict, open('features/clip_%s_%s.json'%(model_nms[clip_nm], split),'w'))
---
+++ utils/preprocessing.py
@@ -0,0 +1,89 @@
+import re
+
+import torch
+import math
+
+from ekphrasis.classes.preprocessor import TextPreProcessor
+from ekphrasis.classes.tokenizer import SocialTokenizer
+from ekphrasis.dicts.emoticons import emoticons
+
+def process_tweet(tweet, text_processor):
+
+    proc_tweet = text_processor.pre_process_doc(tweet)
+
+    clean_tweet = [word.strip() for word in proc_tweet if not re.search(r"[^a-z0-9.,!:()|'\"?%\-\s]+", word)]
+
+    clean_tweet = [word for word in clean_tweet if word not in ['rt', 'http', 'https', 'htt']]
+
+    clean_tweet = " ".join(clean_tweet)
+    clean_tweet = re.sub('\s+', ' ', clean_tweet)
+    clean_tweet = re.sub(' [.]', '.', clean_tweet)
+    clean_tweet = re.sub(' [,]', ',', clean_tweet)
+    clean_tweet = re.sub(' [?]', '?', clean_tweet)
+    clean_tweet = re.sub('[(] ', '(', clean_tweet)
+    clean_tweet = re.sub(' [)]', ')', clean_tweet)
+    clean_tweet = re.sub(" [-] ", "-", clean_tweet)
+
+    return clean_tweet
+
+
+
+def get_text_processor(word_stats='twitter', keep_hashtags=False):
+    return TextPreProcessor(
+            # terms that will be normalized , 'number','money', 'time','date', 'percent' removed from below list
+            normalize=['url', 'email', 'phone', 'user'],
+            # terms that will be annotated
+            annotate={"hashtag","allcaps", "elongated", "repeated",
+                      'emphasis', 'censored'},
+            fix_html=True,  # fix HTML tokens
+
+            # corpus from which the word statistics are going to be used
+            # for word segmentation
+            segmenter=word_stats,
+
+            # corpus from which the word statistics are going to be used
+            # for spell correction
+            corrector=word_stats,
+
+            unpack_hashtags=keep_hashtags,  # perform word segmentation on hashtags
+            unpack_contractions=True,  # Unpack contractions (can't -> can not)
+            spell_correct_elong=True,  # spell correction for elongated words
+
+            # select a tokenizer. You can use SocialTokenizer, or pass your own
+            # the tokenizer, should take as input a string and return a list of tokens
+            tokenizer=SocialTokenizer(lowercase=True).tokenize,
+
+            # list of dictionaries, for replacing tokens extracted from the text,
+            # with other expressions. You can pass more than one dictionaries.
+            dicts=[emoticons]
+        )
+
+
+
+def clip_tokenize(text, _tokenizer, context_length: int = 77):
+
+    sot_token = _tokenizer.encoder["<|startoftext|>"]
+    eot_token = _tokenizer.encoder["<|endoftext|>"]
+    tokens = [sot_token] + _tokenizer.encode(text)[:75] + [eot_token]
+    result = torch.zeros(context_length, dtype=torch.long)
+    result[:len(tokens)] = torch.tensor(tokens)
+
+    return result.unsqueeze(0)
+
+
+def clip_tokenize_ocr(text, _tokenizer, context_length: int = 77):
+
+    all_tokens = _tokenizer.encode(text)
+    k = min(math.ceil(len(all_tokens)/75.0), 2)
+
+    sot_token = _tokenizer.encoder["<|startoftext|>"]
+    eot_token = _tokenizer.encoder["<|endoftext|>"]
+    all_results = []
+    for i in range(k):
+        tokens = [sot_token] + all_tokens[i*75: (i+1)*75] + [eot_token]
+        result = torch.zeros(context_length, dtype=torch.long)
+        result[:len(tokens)] = torch.tensor(tokens)
+
+        all_results.append(result.unsqueeze(0))
+    
+    return all_results
